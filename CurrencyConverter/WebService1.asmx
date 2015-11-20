a4bb587bf9ba6c152911568db71a3a8d29c1aab2
antjo <anthon92.johansson@gmail.com>
2015-11-20 13:19:06 +0000

    now it should work again

---
+++ CurrencyConverter/WebService1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService1.asmx.cs" Class="CurrencyConverter.WebService1" %>
--- CurrencyConverter/startpage.aspx.cs
+++ CurrencyConverter/startpage.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Globalization;
-using System.Collections.Generic;
+
 
 using System.Net;
 using System.Web.Script.Serialization;
@@ -22,43 +22,15 @@ namespace CurrencyConverter
                 protected void ConvertButton_Click(object sender, EventArgs e)
         {
 
-            // Response.RedirectPermanent("convertpage.aspx");
-            decimal rate_from_api = 0;
 
-            decimal amount_of_money = 1;
 
-            rate_from_api = get_rate_from_api(amount_of_money, ddlfrom.SelectedValue, ddlto.SelectedValue);
-            TextBox1.Text = System.Convert.ToString(rate_from_api * System.Convert.ToDecimal(money_text_box.Text));
-        }
+            localhost.WebService1 mywebService = new localhost.WebService1();
+            TextBox1.Text = mywebService.Curr_converter(ddlfrom.SelectedValue, ddlto.SelectedValue, System.Convert.ToDecimal(money_text_box.Text));
 
-        private decimal get_rate_from_api(decimal amount_of_money, string from_curr_code, string to_curr_code)
-
-        {
-
-            try
-
-            {
-                //
-                const string urlPattern = "http://finance.yahoo.com/d/quotes.csv?s={0}{1}=X&f=l1";
-
-                string url = String.Format(urlPattern, from_curr_code, to_curr_code);
-
-                string response = new WebClient().DownloadString(url);
-
-                decimal exchangeRate = decimal.Parse(response, System.Globalization.CultureInfo.InvariantCulture);
-
-                return exchangeRate;
-
-            }
-
-            catch (Exception ex)
-            {
-
-                return 0;
-
-            }
 
         }
+
+
 
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
